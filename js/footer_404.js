0bad32d3442546a3bdfe2f0bd4ea972237614002
mikledotcom <59368373+mikledotcom@users.noreply.github.com>
2026-01-29 21:22:05 +0700

    Update to 2026 footer_404.js

--- js/footer_404.js
+++ js/footer_404.js
@@ -1,5 +1,5 @@
 
-document.write(" <p>© 2012 - 2025 г. Сайт <a target=\"_blank\" href=\"https://www.nivapechka.com\">nivapechka.com</a>. По всем вопросам пишите по адресу nivapechka@gmail.com  </p>\
+document.write(" <p>© 2012 - 2026 г. Сайт <a target=\"_blank\" href=\"https://www.nivapechka.com\">nivapechka.com</a>. По всем вопросам пишите по адресу nivapechka@gmail.com  </p>\
 <a target=\"_blank\" href=\"../../wordpress/legal_ru.html\">Юридическая информация: </a>\
 Копирование материалов сайта возможно только с письменного согласия \
 автора. Полное или частичное использование материалов сайта в сети \
